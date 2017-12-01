a44d302cc08894632dd4aad3e3adfd9291348940
Ndumiso <ndumiso.mhlongo@supergrp.com>
2017-12-01 10:59:25 +0200

    prject commit

---
+++ spring_maven/src/main/java/com/psybergate/spring/App.java
@@ -0,0 +1,13 @@
+package com.psybergate.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
